4e1d514487a5a05cf023b1aab20638688074696e
Caleb <luicelcs@gmail.com>
2021-05-24 19:13:52 -0500

    delete nested old version directory

--- Tranquility/Assets/Scripts/Game/Obstacles/Corruption.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Corruption : MonoBehaviour {
-    // TODO(?)
-    // No longer using this class but keeping it around temporary just incase.
-}
